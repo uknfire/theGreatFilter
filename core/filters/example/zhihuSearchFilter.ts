93c69fadb6ef736124d5dae63c84eea9713f1e94
uknfire <>
2022-06-20 09:52:21 +0800

    refactor: refactor zhihuSearchFilter

--- core/filters/example/zhihuSearchFilter.ts
+++ core/filters/example/zhihuSearchFilter.ts
@@ -1,45 +1,29 @@
-const logger = (...args: any[]) => {
+function logger(...args: any[]) {
     console.log("%c TGF[search]:", 'color: red', ...args)
 }
 
-interface Item {
-    title: string | undefined
-    hasAuthor: boolean
-    isAuthorAnonymous: boolean
-    author: string | undefined
-    type: string | undefined
-    isSearchResult: boolean
-    hasVideo: boolean
-    authorFollowerCount: number
-    authorVoteupCount: number
-    subtype: string | undefined
-    isVerified: boolean
-}
-
-/**
- * normalize the result of zhihu search
- * @param result the result of zhihu search
- */
-function getItemFromSearchResult(result: any): Item {
-    let title: string | undefined = result.object?.title ?? result.object?.question?.name
+function normalizeSearchItem(searchItem: any) {
+    let title: string | undefined = searchItem.object?.title ?? searchItem.object?.question?.name
     title = title?.replaceAll("<em>", '').replaceAll("</em>", '')
+    const author = searchItem.object?.author?.name === undefined ? undefined : {
+        name: searchItem.object.author.name as string,
+        isAnonymous: searchItem.object?.author?.url_token === "0",
+        isVerified: searchItem.object?.author?.badge_v2?.title === '已认证帐号',
+        followerCount: (searchItem.object?.author?.follower_count ?? 0) as number,
+        voteupCount: (searchItem.object?.author?.voteup_count ?? 0) as number,
+    }
     return {
         title,
-        type: result.type,
-        subtype: result.object?.type,   //  "answer", "article" etc.
-        hasAuthor: result.object?.author !== undefined,
-        isAuthorAnonymous: result.object?.author?.url_token === "0",
-        author: result.object?.author?.name,
-        authorFollowerCount: result.object?.author?.follower_count ?? 0,
-        authorVoteupCount: result.object?.author?.voteup_count ?? 0,
-        isSearchResult: result.type === "search_result",
-        hasVideo: result.object?.attachment?.type === "video",
-        isVerified: result.object?.author?.badge_v2?.title === '已认证帐号',
+        type: searchItem.type as string | undefined,
+        subtype: searchItem.object?.type as string | undefined,   //  "answer", "article" etc.
+        isSearchResult: searchItem.type === "search_result",
+        hasVideo: searchItem.object?.attachment?.type === "video",
+        author,
     }
 }
 
-function predicate(obj: any) {
-    const item = getItemFromSearchResult(obj)
+function predicate(searchItem: any) {
+    const item = normalizeSearchItem(searchItem)
     if (!item.isSearchResult) {
         logger(`移除非搜索结果：${item.title} ${item.type}`)
         return false
@@ -48,11 +32,11 @@ function predicate(obj: any) {
         logger(`移除视频回答：${item.title}`)
         return false
     }
-    if (item.hasAuthor && !item.isAuthorAnonymous && item.authorFollowerCount < 100) {
-        logger(`移除低关注用户：${item.title} ${item.author} 关注数${item.authorFollowerCount}`)
+    if (item.author && !item.author.isAnonymous && item.author.followerCount < 100) {
+        logger(`移除低关注用户：${item.title} ${item.author} 关注数`, item.author.followerCount)
         return false
     }
-    if (['九章算法'].includes(item.author!)) {
+    if (item.author && ['九章算法'].includes(item.author.name)) {
         logger(`移除营销号：${item.title} ${item.author}`)
         return false
     }
@@ -68,6 +52,9 @@ function predicate(obj: any) {
     return true
 }
 
+/**
+ * https://www.zhihu.com/search?q=keyword&type=content
+ */
 export const zhihuSearchFilter: Filter = {
     name: 'zhihuSearchFilter',
     isMatch: url => url.startsWith("https://www.zhihu.com/api/v4/search_v3"),
